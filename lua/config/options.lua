5b4fdb7ac3a2ac6759d74f20c73fe5c89f3333a0
JefferyWang <admin@wangjunfeng.com>
2024-08-06 16:14:10 +0800

    add options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+-- 允许复制到系统剪贴板
+opt.clipboard = "unnamedplus"
